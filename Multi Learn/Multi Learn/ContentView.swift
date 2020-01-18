3febd92ae6fea5e91de70acd28e0542d7a00e5ce
Kevin Ngo <ngokevin39@gmail.com>
2020-01-18 00:35:56 -0500

    implemented multiplication table learning game

--- Multi Learn/Multi Learn/ContentView.swift
+++ Multi Learn/Multi Learn/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Multi Learn
 //
-//  Created by Kevin Ngo on 2020-01-15.
+//  Created by Kevin Ngo on 2020-01-16.
 //  Copyright © 2020 Kevin Ngo. All rights reserved.
 //
 
@@ -15,45 +15,74 @@ extension UIScreen{
 }
 
 struct ContentView: View {
-    @State private var numQuestions = 0
+    //make showingHome 2way binding
+    //pass the number of questions
+    @State private var showingHome = true
+    @State private var numberOfQuestionsIndex = 0
+    @State private var multiplicand = 0
+    @State private var numberOfQuestions:Int = 0
+    var numOfQuestionsArray = ["5","10","15","20"]
+    
     
     var body: some View {
-        
-        NavigationView {
-            VStack{
-                Stepper(value: $numQuestions, in: 0...50, label: { Text("Questions:  \(numQuestions)")}).padding(60).font(.title)
-
-                
-                VStack(spacing:0) {
-                    ForEach(0 ..< 4) { row in
-                        HStack(spacing: 0) {
-                            ForEach(0 ..< 4) { column in
-                                NavigationLink(destination: MultiplicationView(numQuestions: self.$numQuestions, multiplicationFactor: (row*4 + column))) {
-                                    Text("\(row*4 + column)")
-                                        .frame(width: UIScreen.screenWidth/4)
-                                        .background(Color.yellow)
-                                        
-                                        .font(.largeTitle)
-                                    
+        NavigationView{
+        Group {
+            if self.showingHome {
+                Group {
+                    VStack(spacing:0) {
+                        Text("How many questions would you like to be asked?")
+                            .padding(.top,(UIScreen.screenHeight/15))
+                        
+                        Picker(selection: $numberOfQuestionsIndex, label: Text("How many questions?")) {
+                            ForEach(0..<numOfQuestionsArray.count) { index in
+                                Text(self.numOfQuestionsArray[index]).tag(index)
+                            }
+                        }.pickerStyle(SegmentedPickerStyle())
+                            .padding(.bottom,(UIScreen.screenHeight/25))
+                            
+                       Text("What multiplication value do you want to practice?")
+                        ForEach(0 ..< 4) { row in
+                            HStack(spacing: 0) {
+                                ForEach(1 ..< 4) { column in
+                                    Button(action: {
+                                        self.toggleView()
+                                        self.multiplicand = (row*3 + column)
+                                    }) {
+                                        Text("\(row*3 + column)")
+                                            .foregroundColor(Color.black)
+                                            .frame(width: (UIScreen.screenWidth/3),height:(UIScreen.screenHeight/8))
+                                            .background(Color.yellow)
+                                            .font(.largeTitle)
+                                            .border(Color.black)
+                                    }
                                 }
-                                
-                                
-                                
-                                
                             }
                         }
+                        .background(Color.orange)
+                        
+                        Spacer()
                     }
-                .padding()
-                    .background(Color.red)
+                    
+                    
                 }
-                Spacer()
-                
             }
-            .navigationBarTitle("Learn Multiplication", displayMode: .inline)
+            else {
+                Group {
+                    MultiplicationView(numberOfQuestions: numberOfQuestions,
+                             multiplicand: multiplicand,
+                             showingHome: $showingHome)
+                }
+            }
         }
-        
-        
+        .navigationBarTitle("Multiplication Practice", displayMode: .inline)
     }
+    }
+    
+    func toggleView() {
+        showingHome = false
+        numberOfQuestions = Int(numOfQuestionsArray[numberOfQuestionsIndex]) ?? 5
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -61,3 +90,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
--- Multi Learn/Multi Learn/MultiplicationView.swift
+++ Multi Learn/Multi Learn/MultiplicationView.swift
@@ -1,41 +1,135 @@
 //
-//  MultiplicationView.swift
+//  GameView.swift
 //  Multi Learn
 //
-//  Created by Kevin Ngo on 2020-01-15.
+//  Created by Kevin Ngo on 2020-01-16.
 //  Copyright © 2020 Kevin Ngo. All rights reserved.
 //
 
 import SwiftUI
 
+//todo how to put display questionanswer in text
+
+struct QuestionAnswer {
+    var multiplicant: Int
+    var answer: Int
+    
+}
+
+
 struct MultiplicationView: View {
-    @Binding var numQuestions: Int
-    @State var multiplicationFactor: Int
+    
+    //TODO: logic could be cleaned up
+    @State private var solution = ""
+    @State private var numberOfCorrect = 0
+    let numberOfQuestions: Int
+    let multiplicand: Int
+    @State var questionsAnswers: [QuestionAnswer] = []
+    @Binding var showingHome: Bool
+    @State var isGameOver: Bool = false
+    
+    init(numberOfQuestions: Int, multiplicand: Int, showingHome: Binding<Bool>) {
+        self.numberOfQuestions = numberOfQuestions
+        self.multiplicand = multiplicand
+        self._showingHome = showingHome
+    }
+    
     var body: some View {
         Group {
-        VStack {
-            Text("Questions Remaining: \(numQuestions)")
-            Text("Multiplication Factor: \(multiplicationFactor)")
-            Button(action: {
-                self.numQuestions -= 1
-            }) {
-                Text("chng")
+            VStack {
+                Text("Num correct: \(String(numberOfCorrect))")
+                    .foregroundColor(Color.green)
+                
+                HStack {
+                    Spacer()
+                    
+                    Text("\(String(multiplicand))")
+                        .frame(width: (UIScreen.screenWidth/5),height:(UIScreen.screenHeight/8))
+                        .font(.system(size: 60))
+                        .padding()
+                    
+                    Spacer()
+                    
+                    Text("x")
+                        .font(.largeTitle)
+                    
+                    Spacer()
+                    
+                    Text("\(getMultiplicant())")
+                        .frame(width: (UIScreen.screenWidth/5),height:(UIScreen.screenHeight/8))
+                        .font(.system(size: 60))
+                        .padding()
+                    
+                    Spacer()
+                }.padding()
+                .padding(.top, (UIScreen.screenHeight/30))
+                
+                VStack(spacing: 0) {
+                    TextField("Answer", text: $solution)
+                        .keyboardType(.numberPad)
+                        .padding()
+                        .frame(width: (UIScreen.screenWidth),height:(UIScreen.screenHeight/16))
+                        .border(Color.black)
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                        
+                    
+                    Button(action: {
+                        self.checkAnswer()
+                    }){
+                        Text("Submit")
+                        .font(.title)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                        .border(Color.black)
+                    }
+                    
+                    }.padding()
+                    .cornerRadius(5)
+                Spacer()
+            }
+        }.onAppear{
+            var multiplicant = 0
+            for _ in 0 ..< self.numberOfQuestions {
+                multiplicant = Int.random(in: 1..<13)
+                self.questionsAnswers.append(QuestionAnswer(multiplicant: multiplicant, answer: multiplicant*self.multiplicand))
             }
         }
-            Spacer()
+        .alert(isPresented: $isGameOver) {
+            Alert(title: Text("Game Over"),
+                  message: Text("You got \(String(numberOfCorrect))/\(String(numberOfQuestions)) correct."),
+                  dismissButton: .default(Text("Confirm")) {
+                    self.showingHome = true
+                })
         }
     }
     
-    //returns the (multiplicand, solution)
-    func createEquation() -> (Int, Int) {
-        let randomInt = Int.random(in: 1..<15)
-        let solution = multiplicationFactor * randomInt
-        return (randomInt, solution)
+    func checkAnswer() {
+        if Int(solution) == questionsAnswers[0].answer {
+            numberOfCorrect += 1
+        }
+        if questionsAnswers.count > 1 {
+            questionsAnswers.remove(at:0)
+        } else {
+            isGameOver = true
+        }
+        solution = ""
+    }
+    
+    
+    func getMultiplicant() -> String {
+        if questionsAnswers.count > 0 {
+            return String(questionsAnswers[0].multiplicant)
+        }
+        return ""
     }
 }
 
-struct MultiplicationView_Previews: PreviewProvider {
+struct GameView_Previews: PreviewProvider {
     static var previews: some View {
-        MultiplicationView(numQuestions: .constant(4), multiplicationFactor: 4)
+        MultiplicationView(numberOfQuestions:5, multiplicand: 1, showingHome: .constant(false))
+        
     }
 }
