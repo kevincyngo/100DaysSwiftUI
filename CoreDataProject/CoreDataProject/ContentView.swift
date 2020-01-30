1e7a7196b69c5623217fa4105dca89d1fb12b2ca
Kevin Ngo <ngokevin39@gmail.com>
2020-01-29 20:22:57 -0500

    filtering using NSPredicate

---
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Kevin Ngo on 2020-01-29.
+//  Copyright © 2020 Kevin Ngo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "universe == %@", "Star Wars")) var ships: FetchedResults<Ship>
+
+    var body: some View {
+        VStack {
+            List(ships, id: \.self) {ship in
+                Text(ship.name ?? "Unknown name")
+            }
+            
+            Button("Add examples") {
+                let ship1 = Ship(context: self.moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+
+                let ship2 = Ship(context: self.moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+
+                let ship3 = Ship(context: self.moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+
+                let ship4 = Ship(context: self.moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+
+                try? self.moc.save()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
