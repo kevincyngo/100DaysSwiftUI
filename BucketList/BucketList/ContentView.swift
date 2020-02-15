2e7d9309302638c3c0a5fccf2d4470dbc68473f9
Kevin Ngo <ngokevin39@gmail.com>
2020-02-14 19:16:38 -0500

    BucketList initial implementaiton

---
+++ BucketList/BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Kevin Ngo on 2020-02-14.
+//  Copyright © 2020 Kevin Ngo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
