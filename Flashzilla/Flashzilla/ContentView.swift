b445fa3e3c1467e6e4e0b241d394adacfdf75c7d
Kevin Ngo <ngokevin39@gmail.com>
2020-02-23 21:47:25 -0500

    Flashzilla initial commit

---
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Kevin Ngo on 2020-02-23.
+//  Copyright © 2020 Kevin Ngo. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+import CoreHaptics
+
+struct ContentView: View {
+    
+    @State private var engine: CHHapticEngine?
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        // create one intense, sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    var body: some View {
+        Text("Hello, World!")
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
+        
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
