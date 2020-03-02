e1d469617b854815c5c1fe0cd9c40beee2c093b3
Kevin Ngo <ngokevin39@gmail.com>
2020-03-01 21:46:31 -0500

    SnowSeeker implementation

---
+++ SnowSeeker/SnowSeeker/View/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Kevin Ngo on 2020-02-27.
+//  Copyright © 2020 Kevin Ngo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    @ObservedObject var favorites = Favorites()
+
+    
+    var body: some View {
+        NavigationView {
+            List(resorts) { resort in
+                NavigationLink(destination: ResortView(resort: resort)) {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 25)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 5)
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                    .layoutPriority(1)
+                }
+                if self.favorites.contains(resort) {
+                    Spacer()
+                    Image(systemName: "heart.fill")
+                    .accessibility(label: Text("This is a favorite resort"))
+                        .foregroundColor(.red)
+                }
+            }
+            .navigationBarTitle("Resorts")
+            
+            WelcomeView()
+        }.environmentObject(favorites)
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SnowSeeker/SnowSeeker/View/ResortDetailsView.swift
@@ -0,0 +1,40 @@
+//
+//  ResortDetailsView.swift
+//  SnowSeeker
+//
+//  Created by Kevin Ngo on 2020-02-27.
+//  Copyright © 2020 Kevin Ngo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ResortDetailsView: View {
+    let resort: Resort
+    var size: String {
+        switch resort.size {
+        case 1:
+            return "Small"
+        case 2:
+            return "Average"
+        default:
+            return "Large"
+        }
+    }
+    var price: String {
+        String(repeating: "$", count: resort.price)
+    }
+    var body: some View {
+        Group {
+            Text("Size: \(size)").layoutPriority(1)
+            Spacer().frame(height: 0)
+
+            Text("Price: \(price)").layoutPriority(1)
+        }
+    }
+}
+
+struct ResortDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResortDetailsView(resort: Resort.example)
+    }
+}
